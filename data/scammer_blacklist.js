d6c9842578ba8c18d5f35f8507ba56245f5a46fe
gilberto978 <gilabraham10@gmail.com>
2025-09-13 22:25:21 +0300

    Update and rename regulator_blacklist.js to scammer_blacklist.js

---
+++ data/scammer_blacklist.js
@@ -0,0 +1,7 @@
+// scammer_blacklist.js
+module.exports = [
+  { domain: "rolex-deals247.com", source: "Watchuseek Scammer List", date: "2025-09-10" },
+  { domain: "swissluxuryshop.net", source: "Rolex Forums Reports", date: "2025-09-08" },
+  { domain: "prestigetime-swiss.co", source: "Community Complaints", date: "2025-09-06" },
+  // GitHub Action will auto-update this weekly
+];
