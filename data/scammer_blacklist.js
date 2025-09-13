b9dc737f90123576560e4e23d2b889a92c38141e
gilberto978 <gilabraham10@gmail.com>
2025-09-13 22:34:48 +0300

    Update scammer_blacklist.js

--- data/scammer_blacklist.js
+++ data/scammer_blacklist.js
@@ -1,7 +1,14 @@
-// scammer_blacklist.js
-module.exports = [
-  { domain: "rolex-deals247.com", source: "Watchuseek Scammer List", date: "2025-09-10" },
-  { domain: "swissluxuryshop.net", source: "Rolex Forums Reports", date: "2025-09-08" },
-  { domain: "prestigetime-swiss.co", source: "Community Complaints", date: "2025-09-06" },
-  // GitHub Action will auto-update this weekly
+export default [
+  {
+    domain: "rolex-deals247.com",
+    source: "Watchuseek Scammer List",
+    date: "2025-09-10",
+    url: "#",
+  },
+  {
+    domain: "swissluxuryshop.net",
+    source: "Rolex Forums Reports",
+    date: "2025-09-08",
+    url: "#",
+  },
 ];
